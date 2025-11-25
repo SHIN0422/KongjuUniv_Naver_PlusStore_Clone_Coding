85aa57aa00ff5400015b97aa073626d33cc0d97d
min gyu jang <programmingyu617@gmail.com>
2025-11-25 17:45:01 +0900

    first commit

---
+++ src/main/java/com/example/DB_NAVER_PLUSSTORE_CLONECODDING/DbNaverPlusstoreClonecoddingApplication.java
@@ -0,0 +1,13 @@
+package com.example.DB_NAVER_PLUSSTORE_CLONECODDING;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbNaverPlusstoreClonecoddingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbNaverPlusstoreClonecoddingApplication.class, args);
+	}
+
+}
